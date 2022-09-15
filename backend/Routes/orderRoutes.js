803c1dfe5a807114b372940387361acc709357c2
Yogesh Kumar <64686788+void-lucifer@users.noreply.github.com>
2022-09-15 21:11:50 +0530

    Delete backend directory

--- backend/Routes/orderRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const orderController = require('../Controllers/orderController')
-
-router.get('/checkout/:userId', orderController.getCheckout);
-router.get('/clear-cart/:userId', orderController.deleteAllCartItems);
-module.exports = router
